cab3fff753d59c27aae60174ffd8f1ccd0e09225
Ray Brooks <ray.userinterface@gmail.com>
2022-12-12 23:53:23 +0000

    agent does not have backend! try disabling bt and wifi to fix pi network hang

--- scripts/install-cloud-init.sh
+++ scripts/install-cloud-init.sh
@@ -47,5 +47,10 @@ ssh_authorized_keys:
   - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCuUOzRM1sPenNhmf75aU473eNoH/UGcMfJKITGCXpxayP9dGWyytjpKa1Xuu4TJXHtqqE/dpZSo6TlmoK0pmOR0bt0Knp0YslriNBRL+hRsqYcKat8vSOhUSz0otxBR3eC5x4/7jBBuwLGNM3tyQL/3pGDw9FVntJ1clqRW881F6r7qcan5VOkyviwtYldG0haIV2XoQ7GMB2AJytokj/xrSJBlNEzda4WH9MwpnO8zF2YSoHsdh+qNAzwFvvI2+34CDk3oBM/vzi48dHjVm/a3S6ymCL/zlfMKjrFUgSAqrlz8JNJwp0v43Lf2W1+vEUZAFSDil18VFZS3WkkiMbV8vHYzwVEh2u3PFmcV4IHooam7zNeSL4mGRHIM4F0+9PJ80adQsBH1UGcw+7K926b4WrJWXtolh+g2wDjSmJY/moAGLoiQXtoeBYiw9S7FeNaR+gLYfFgtq0s81C0uCBus3L5a8a4u+oth4D5e1YRHqBFGOmKt1qKwu99xpBFePawf/VFp8s4yEf+koe/oX1hdLUnYHaap/EvX2M0Tb0Y1NMEpfcLwUx1IiGlCdQ7Cu4OA0PVMYartcWi291CigxSC65lkl4iUQejtv9tqKFW+tfA9iITpKSOeyXJbhWpMOTgNygXpMiqNq4s14t+1S3A0NsEyzRei0FE4Cs3Qo5N2w== ray.userinterface@gmail.com
 EOF
 
+printf "\n\
+dtoverlay=disable-wifi\n\
+dtoverlay=disable-bt\n\
+" >> /boot/config.txt
+
 # Disable dhcpcd - it has a conflict with cloud-init network config
 systemctl mask dhcpcd
