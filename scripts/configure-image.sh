40c2df4e4819995f315e45853411373eea5c7fb4
Ray Brooks <ray.userinterface@gmail.com>
2023-07-24 13:03:03 +0100

    modularise dnsmasq.conf

--- scripts/configure-image.sh
+++ scripts/configure-image.sh
@@ -1,29 +1,36 @@
 MASTER_HOSTNAME=buffy
 
+#loop device for mounting agent image
 LOOP_DEV=/dev/loop0
 
+#base files and directories
+LIBBASE=/var/lib/scooby
+AGENTCONFIGBASE=/etc/scooby/agents
+AGENTINSTANCEBASE=${LIBBASE}/agents
+AGENTMOUNTBASE=/mnt/scooby/agents
+AGENTSSHBASE=${LIBBASE}/ssh
+IMAGEBASE=${LIBBASE}/images
+BASEPATH=${LIBBASE}/base
+OVERLAYWORKPATH=${LIBBASE}/overlay
+RANCHERSTORAGEPATH=/var/lib/rancher
+VAGRANT=/vagrant
+AGENTIMAGEBASE=${IMAGEBASE}/agent-scooby.img
+
+#dist image mount points for vagrant
 BOOT_MNT=/tmp/boot
 ROOT_MNT=/tmp/root
 
-AGENTCONFIGBASE=/etc/scooby/agents
-AGENTINSTANCEBASE=/var/lib/scooby/agents
-AGENTMOUNTBASE=/mnt/scooby/agents
-BASEPATH=/var/lib/scooby/base
-
+#agent image mount paths for vagrant
 AGENTCONFIGPATH=${ROOT_MNT}${AGENTCONFIGBASE}
-AGENTINSTANCEPATH=${ROOT_MNT}${AGENTINSTANCEBASE}
 AGENTMOUNTPATH=${ROOT_MNT}${AGENTMOUNTBASE}
 CLOUDCONFIGWWWPATH=${ROOT_MNT}/var/www/html/cloud-config
 
-OVERLAYWORKPATH=/var/lib/scooby/overlay
-RANCHERSTORAGEPATH=/var/lib/rancher
-
-VAGRANT=/vagrant
-IMGPATH=${VAGRANT}/images/scooby.img
-SERVERCONFIGPATH=${VAGRANT}/server
+#agent image mount point for server on first boot
+BOOT_MNT_LOCAL=/mnt/boot
+ROOT_MNT_LOCAL=/mnt/root
 
 #MOUNT IMAGE AS LOOP DEVICE
-losetup -Pf ${IMGPATH}
+losetup -Pf ${VAGRANT}/images/scooby.img
 mkdir -p ${BOOT_MNT}
 mkdir -p ${ROOT_MNT}
 mount ${LOOP_DEV}p1 ${BOOT_MNT}
@@ -105,30 +112,33 @@ EOF
 chmod a+x ${BOOT_MNT}/user-data
 
 ### COPY STATIC CONFIG
-rsync -x --progress -r ${SERVERCONFIGPATH}/* ${ROOT_MNT}
+rsync -x --progress -r ${VAGRANT}/server/* ${ROOT_MNT}
 echo "COPIED STATIC CONFIG"
 
 ### BASE SERVICES CONFIG
 ## INTERNAL DNSMASQ
-DHCP_RANGE=$(echo ${LC_INTERNAL_IP} | grep -Eo '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}')
 cat - > ${ROOT_MNT}/etc/dnsmasq.conf << EOF
 port=0
-domain=${LC_INTERNAL_DOMAIN}
-
-interface=${LC_INTERNAL_DEVICE}
 bind-interfaces
 dhcp-authoritative
 
 expand-hosts
 log-dhcp
-#dhcp-ignore=tag:!known
+EOF
 
-dhcp-range=tag:${LC_INTERNAL_DEVICE},${DHCP_RANGE}192,${DHCP_RANGE}254
+#ASSUME CLASS C NETWORK FOR NOW
+INTERNAL_NET=$(grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}' ${LC_INTERNAL_IP})
+
+cat - > ${ROOT_MNT}/etc/dnsmasq.d/10-scooby-dhcp.conf << EOF
+domain=${LC_INTERNAL_DOMAIN}
+interface=${LC_INTERNAL_DEVICE}
+
+dhcp-range=tag:eth1,${INTERNAL_NET},static
 dhcp-option=tag:${LC_INTERNAL_DEVICE},66,${LC_INTERNAL_IP}
 dhcp-option=tag:${LC_INTERNAL_DEVICE},option:router,${LC_INTERNAL_IP}
 dhcp-option=tag:${LC_INTERNAL_DEVICE},6,${LC_EXTERNAL_DNS}
 
-enable-tftp
+enable-tftp=${LC_INTERNAL_DEVICE}
 tftp-root=/tftpboot/
 pxe-service=0,"Raspberry Pi Boot"
 EOF
@@ -197,21 +207,72 @@ publish-hinfo=no
 publish-workstation=no
 EOF
 
-### ENV
-mkdir -p ${ROOT_MNT}/etc/scooby
-cat - > ${ROOT_MNT}/etc/scooby/.env << EOF
-  LC_EXTERNAL_IP=${LC_EXTERNAL_IP}
-  LC_INTERNAL_IP=${LC_INTERNAL_IP}
-  LC_INTERNAL_DEVICE=${LC_INTERNAL_DEVICE}
-  LC_DEFAULT_USER=${LC_DEFAULT_USER}
-  LC_AGENT_IMAGE_HREF="${LC_AGENT_IMAGE_HREF}"
-EOF
-echo "CREATED ENV FILE"
-
 ### HOSTS
 cat - >> ${ROOT_MNT}/etc/hosts << EOF
   ${LC_INTERNAL_IP} ${MASTER_HOSTNAME}
 EOF
+
+### FIRST BOOT CONFIGURATION
+cat - >> ${ROOT_MNT}/usr/local/bin/finalize-cloud-init.sh << EOF
+#!/bin/sh
+
+#SSH KEYS
+ssh-keygen -q -f /home/${LC_DEFAULT_USER}/.ssh/id_ed25519 -N "" -t ed25519
+cat /home/${LC_DEFAULT_USER}/.ssh/id_ed25519.pub >> /home/${LC_DEFAULT_USER}/.ssh/authorized_keys
+chown -R ${LC_DEFAULT_USER}:${LC_DEFAULT_USER} /home/${LC_DEFAULT_USER}/.ssh/
+
+for AGENT in \$(cd ${AGENTCONFIGBASE}; ls -d *)
+do
+  ### COPY AGENT KEYS FOR K3S
+  mkdir -p ${AGENTCONFIGBASE}/\${AGENT}${AGENTSSHBASE}/
+  rsync -xa --progress /home/${LC_DEFAULT_USER}/.ssh/id_ed25519 ${AGENTCONFIGBASE}/\${AGENT}${AGENTSSHBASE}
+  rsync -xa --progress /home/${LC_DEFAULT_USER}/.ssh/id_ed25519.pub ${AGENTCONFIGBASE}/\${AGENT}${AGENTSSHBASE}
+  cat /home/${LC_DEFAULT_USER}/.ssh/id_ed25519.pub > ${AGENTCONFIGBASE}/\${AGENT}${AGENTSSHBASE}/authorized_keys
+done
+
+#GET AGENT IMAGE FROM AWS
+mkdir -p ${IMAGEBASE}
+curl -o ${AGENTIMAGEBASE} "${LC_AGENT_IMAGE_HREF}"
+
+#MOUNT IMAGE AS LOOP DEVICE
+losetup -Pf ${AGENTIMAGEBASE}
+mkdir -p ${BOOT_MNT_LOCAL}
+mkdir -p ${ROOT_MNT_LOCAL}
+mount ${LOOP_DEV}p1 ${BOOT_MNT_LOCAL}
+mount ${LOOP_DEV}p2 ${ROOT_MNT_LOCAL}
+
+#COPY IMAGE INTO BASE
+mkdir -p ${BASEPATH}/boot
+rsync -xa --progress -r ${ROOT_MNT_LOCAL}/* ${BASEPATH}
+rsync -xa --progress -r ${BOOT_MNT_LOCAL}/* ${BASEPATH}/boot
+
+#copy bootcode.bin to /tftpboot
+rsync -xa --progress ${BOOT_MNT_LOCAL}/bootcode.bin /tftpboot/
+
+#UNMOUNT LOOP DEVICES
+umount ${BOOT_MNT_LOCAL}
+umount ${ROOT_MNT_LOCAL}
+losetup -D ${LOOP_DEV}
+
+#REMOUNT OVERLAYFS
+umount ${AGENTMOUNTPATH}/*
+mount -a
+
+#REEXPORT NFS
+exportfs -a
+
+#K3S
+cd /tmp && curl -sLS https://get.k3sup.dev | sh
+# https://github.com/k3s-io/k3s/issues/535#issuecomment-863188327
+k3sup install --ip ${LC_INTERNAL_IP} --user ${LC_DEFAULT_USER} --ssh-key "/home/${LC_DEFAULT_USER}/.ssh/id_ed25519" --k3s-extra-args "--node-external-ip=${LC_EXTERNAL_IP} --flannel-iface='${LC_INTERNAL_DEVICE}' --kube-apiserver-arg 'service-node-port-range=1000-32767'"
+
+mkdir -p /root/.kube
+rsync -xa --progress ./kubeconfig /root/.kube/config
+
+#READY
+wall "BUFFY WILL PATROL TONIGHT"
+EOF
+chmod a+x ${ROOT_MNT}/usr/local/bin/finalize-cloud-init.sh  
 
 #CREATE TFTPBOOT DIRECTORY
 
@@ -223,14 +284,13 @@ mkdir -p ${ROOT_MNT}${BASEPATH}/boot
 
 ### AGENT CONFIG
 mkdir -p ${AGENTCONFIGPATH}
-AGENTS=$(find ${AGENTCONFIGPATH}/* -maxdepth 0 -type d -printf "%f\n")
 FSID=1
 
 ####################
 ### AGENT LOOP START
 ####################
 
-for AGENT in ${AGENTS}
+for AGENT in $(cd ${AGENTCONFIGPATH}; ls -d *)
 do
 
 echo "CONFIGURING AGENT ${AGENT}"
@@ -311,25 +371,34 @@ ${LC_INTERNAL_IP}:${AGENTMOUNTBASE}/${AGENT} / nfs defaults 0 0
 UUID="${AGENT_RANCHER_PART_UUID}" ${RANCHERSTORAGEPATH} ext4 defaults 0 0
 EOF
 
-### CREATE ENV
-mkdir -p ${AGENTCONFIGPATH}/${AGENT}/etc/scooby
-cat - > ${AGENTCONFIGPATH}/${AGENT}/etc/scooby/.env << EOF
-LC_INTERNAL_IP=${LC_INTERNAL_IP}
-LC_DEFAULT_USER=${LC_DEFAULT_USER}
-EOF
-
 ### CREATE AGENT DNSMASQ ENTRY
 AGENT_ETHERNET=$(cat ${AGENTCONFIGPATH}/${AGENT}/ethernet)
 AGENT_IP=$(cat ${AGENTCONFIGPATH}/${AGENT}/ip)
-cat - >> ${ROOT_MNT}/etc/dnsmasq.conf << EOF
+cat - > ${ROOT_MNT}/etc/dnsmasq.d/20-scooby-agent-${AGENT} << EOF
 dhcp-host=net:${AGENT},${AGENT_ETHERNET},${AGENT},${AGENT_IP},24h
 dhcp-option=net:${AGENT},17,${LC_INTERNAL_IP}:${AGENTMOUNTBASE}/${AGENT}
 EOF
 
-### COPY BINARIES
+### CREATE AGENT FIRST BOOT CONFIGURATION
+
 mkdir -p ${AGENTCONFIGPATH}/${AGENT}/usr/local/bin
-rsync -xa --progress -r ${SERVERCONFIGPATH}/usr/local/bin/finalize-cloud-init-agent.sh ${AGENTCONFIGPATH}/${AGENT}/usr/local/bin/
-chmod a+x ${AGENTCONFIGPATH}/${AGENT}/usr/local/bin/finalize-cloud-init-agent.sh
+cat - > ${AGENTCONFIGPATH}/${AGENT}/usr/local/bin/finalize-cloud-init-agent.sh << EOF
+#!/bin/sh
+
+cp ${AGENTSSHBASE}/* /home/${LC_DEFAULT_USER}/.ssh/
+
+cd /tmp && curl -sLS https://get.k3sup.dev | sh
+k3sup join \
+--ip ${AGENT_IP} \
+--server-ip ${LC_INTERNAL_IP} \
+--user ${LC_DEFAULT_USER} \
+--server-user ${LC_DEFAULT_USER} \
+--ssh-key "${AGENTSSHBASE}/id_ed25519" \
+--k3s-extra-args "--node-name '${AGENT}' --node-label 'smarter-device-manager=enabled'"
+
+wall "${AGENT} IS ONLINE"
+EOF
+chmod a+x ${AGENTCONFIGPATH}/${AGENT}/usr/local/bin/finalize-cloud-init-agent.sh 
 
 FSID=$((FSID+1))
 
